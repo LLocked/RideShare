6e6a54292d80d78af6732e7a2ff4d8515f677e61
Lucas Hong <lhong27@students.polytechnic.org>
2025-02-24 09:15:30 -0800

    main

---
+++ Person.java
@@ -0,0 +1,29 @@
+public class Person{
+    //fields
+    private int destination;
+    private boolean direction; //true right ;left false
+    private int startLocation;
+    //contructors
+    public Person(int myDestination,int myStart){
+        destination = myDestination;
+        startLocation = myStart;
+        direction = destination> startLocation;
+    }
+    
+    public int getDestination(){
+        return destination;
+    }
+
+    public boolean getDirection(){
+        return direction;
+    }
+
+    public int getStart(){
+        return startLocation;
+    }
+
+    public String toString(){
+        return super.toString()+" Start: " + startLocation + " Destination: " + destination;
+        }
+
+}
