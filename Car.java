7be3f0b1b9da861f932ae57d898d4e731b6bb73d
Lucas Hong <lhong27@students.polytechnic.org>
2025-02-24 09:33:47 -0800

    r

--- Car.java
+++ Car.java
@@ -1,18 +1,29 @@
 import java.util.ArrayList;
+
 public class Car {
     private int destination;
     private int currentLocation;
     private boolean direction;
-    private Arraylist<Person>passengers;
+    private ArrayList<Person>passengers;
 
     public Car(int myDestination, int start){
         destination = myDestination;
         currentLocation = start;
-        direction = direction > currentLocation;
+        direction = destination > currentLocation;
         passengers = new ArrayList<Person>();
     }
 
-    public String({
-        return "Destination: "+destination+" Going right? "+direction+" Passengers: "+passengers+""
-    })
+    public String toString(){
+        return "Destination: "+destination+" Going right? "+direction+" Passengers: "+passengers+"";
+    }
+    public void addPassenger(Person p){
+        if(passengers.size()>= 3){
+            System.out.println("Error: No room!");
+        }else{
+        passengers.add(p);
+        }
+    }
+    public boolean hasRoom(){
+        return passengers.size() < 3;
+    }
 }
--- Tester.java
+++ Tester.java
@@ -3,5 +3,13 @@ public class Tester {
         Person a = new Person(4, 6);
         Person b = new Person(2, 3);
         Person c = new Person(7, 5);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        Car n = new Car(5,9);
+        System.out.println(n);
+        n.addPassenger(a);
+        System.out.println(n);
+
     }
 }
